46f3ea1a58377682b4b6e3b95cd5edbbfa2bd7eb
lenon nascimento <lenoninformatica@gmail.com>
2022-09-26 23:24:48 -0300

    :construction: Requisito 1 feito :heavy_check_mark:

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,49 @@ class Form extends React.Component {
   render() {
     return (
       <>
+        <label htmlFor="name" data-testid="name-input">
+          Nome:
+          <input type="text" />
+        </label>
+        <br />
+        <label htmlFor="textarea" data-testid="description-input">
+          Descricao:
+          <textarea type="textarea" />
+        </label>
+        <br />
+        <label htmlFor="number" data-testid="attr1-input">
+          Attr01:
+          <input type="number" />
+        </label>
+        <br />
+        <label htmlFor="number" data-testid="attr2-input">
+          Attr02:
+          <input type="number" />
+        </label>
+        <br />
+        <label htmlFor="number" data-testid="attr3-input">
+          Attr03:
+          <input type="number" />
+        </label>
+        <br />
+        <label data-testid="image-input">
+          imagem:
+          <input />
+        </label>
+        <br />
+        <select data-testid="rare-input">
+          Raridade:
+          <option value="normal">normal</option>
+          <option value="raro">raro</option>
+          <option value="muito raro">muito raro</option>
+        </select>
+        <br />
+        <label>
+          SuperTrybeTrunfo:
+          <input data-testid="trunfo-input" type="checkbox" />
+        </label>
+        <br />
+        <button data-testid="save-button" type="button">Salvar</button>
       </>
     );
   }
